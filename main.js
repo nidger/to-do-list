66bb66f261c8bc814b84a87da682ed2a2b187099
nidger <nprior70@yahoo.com>
2024-11-05 11:01:54 +0000

    added buttons in markup

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
+// Get our DOM elements
 const taskInput = document.getElementById('taskInput');
-const addButton = document.getElementbyId('addButton');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+const taskTemplate = document.getElementById('taskTemplate');
 
+// Create our tasks array to store all tasks
 let tasks = [];
 
-addButton.addEventListener('click', () => {
-  const taskText = taskInput.value.trim();
-  if (taskText !== '' {
-    console.log('Task to add:', taskText);
-    taskInput.value = '';
-  })
+// Add event listener to the button
+addButton.addEventListener('click', function() {
+    // Get the task text from input
+    const taskText = taskInput.value.trim();
+    
+    // Basic validation - check if input is not empty
+    if (taskText !== '') {
+      const task = {
+        id: Date.now(), //unique identifier
+        text: taskText,
+        status: 'todo' //inital status
+      };
+        // Add task to array
+        tasks.push(task);
+        // Add to DOM
+        addTask(task);
+        
+        // Clear the input field
+        taskInput.value = '';
+    }
 });
 
+function addTask(task) {
+  //Clone the template
+  const taskElement = taskTemplate.content.cloneNode(true);
+
+  //set the task list
+  taskElement.querySelector('.task-text').textContent = task.text;
+
+  //Add to list
+  taskList.appendChild(taskElement);
+}
